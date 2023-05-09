334cbeee57dfeaaebb7a10b88756e2d1a705f4ed
Ouail-01 <ouaillaarabi@gmail.com>
2023-05-09 16:52:54 +0100

    Redux store tutorial

---
+++ src/features/modal/modalSlice.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isOpen: false,
+};
+
+const modalSlice = createSlice({
+  name: 'modal',
+  initialState,
+  reducers: {
+    openModal: (state) => {
+      state.isOpen = true;
+    },
+    closeModal: (state) => {
+      state.isOpen = false;
+    },
+  },
+});
+
+export const { openModal, closeModal } = modalSlice.actions;
+
+export default modalSlice.reducer;
